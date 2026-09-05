16cb1a49fec38f90950fd4d22c7dde703ba701da
Hadixp <hadi.b.2002@gmail.com>
2026-09-06 00:55:53 +0430

    new2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/mern-render">
       <App />
-    </BrowserRouter>
   </StrictMode>,
 );
